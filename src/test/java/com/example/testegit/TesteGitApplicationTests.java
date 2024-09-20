fd5eac33fa5b4760e56146bdc2f37ec37365fc50
AndersonDominguesRS <anderson.domingues@al.infnet.edu.br>
2024-09-20 19:29:16 -0300

    Primeiro commit

---
+++ src/test/java/com/example/testegit/TesteGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
